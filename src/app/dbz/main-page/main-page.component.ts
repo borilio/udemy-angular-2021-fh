340d2eaff8a0dc1aaecf330c6bac39354801a9e0
borilio <exprimetuandroid@gmail.com>
2021-07-28 14:23:12 +0200

    Añadido formulario con FormsModule y ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+    nombre: string;
+    poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+  personajes: Personaje[] = [
+    { nombre: 'Goku', poder: 1200 },
+    { nombre: 'Krillin', poder: 700 },
+    { nombre: 'Vegeta', poder: 1500 },
+    { nombre: 'Yamcha', poder: 300 }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  };
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  agregar(): void {    
+    //Si el nombre NO está vacío, lo agregamos, si si, nos salimos.
+    if (this.nuevo.nombre) {
+      //Lo agregamos
+      this.personajes.push(this.nuevo);
+      //Lo reseteamos
+      this.nuevo = {  
+        nombre: '',
+        poder: 0
+      };
+    } else{
+      alert("El nombre no puede estar vacío");
+    }
+
   }
 
 }
